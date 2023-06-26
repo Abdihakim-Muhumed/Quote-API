16befe56f40aa1829f04a613cdbe404a4baf25ab
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-06-26 12:26:47 +0300

    Added method updateElement

--- utils.js
+++ utils.js
@@ -9,6 +9,19 @@ const getIndexById = (id, elementList) => {
   });
 };
 
+const updateElement = (id, queryArguments, elementList) => {
+  const elementIndex = getIndexById(id, elementList);
+  if (elementIndex === -1) {
+    throw new Error('updateElement must be called with a valid id parameter');
+  }
+  if (queryArguments.id) {
+    queryArguments.id = Number(queryArguments.id);
+  }
+  Object.assign(elementList[elementIndex], queryArguments);
+  return elementList[elementIndex];
+};
+
+
 module.exports = {
   getRandomElement,
   getIndexById
