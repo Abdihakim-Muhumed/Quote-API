a538b602b78d486dae98a3ab649623ddb80718db
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-06-26 11:05:14 +0300

    Added endpoint for a random quote.

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 4001;
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
-const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+// get random quotes;
+app.get('/api/quotes/random', (req, res) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send({
+        quote: randomQuote,
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+});
