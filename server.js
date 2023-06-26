5383674fb5785fe4eb31c1f51a34bfcb10788ac0
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-06-26 11:33:44 +0300

    Added GET endpoint for fetching all quotes and by an author

--- server.js
+++ server.js
@@ -8,13 +8,31 @@ const { getRandomElement } = require('./utils');
 
 app.use(express.static('public'));
 
-// get random quotes;
+// get random quote;
 app.get('/api/quotes/random', (req, res) => {
     const randomQuote = getRandomElement(quotes);
     res.send({
         quote: randomQuote,
     });
 })
+
+// get all quotes
+app.get('/api/quotes', (req, res) => {
+    if(Object.keys(req.query).length === 0){
+        res.send({
+            quotes: quotes
+        });
+    }
+    const requestedQuotes = quotes.filter(quote => quote.person == req.query.person);
+    if(!requestedQuotes){
+        res.send({
+            quotes: []
+        })
+    }
+    res.send({
+        quotes: requestedQuotes
+    })    
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
